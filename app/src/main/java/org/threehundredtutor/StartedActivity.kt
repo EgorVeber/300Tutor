52ac0dded3c3d935f168742c1ce643f107b4f749
JamesOhara <egor.veber.02.07.95@gmail.com>
2023-07-13 23:41:51 +0300

    4543543534

--- app/src/main/java/org/threehundredtutor/StartedActivity.kt
+++ app/src/main/java/org/threehundredtutor/StartedActivity.kt
@@ -1,11 +1,11 @@
 package org.threehundredtutor
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class StartedActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.started_activity_layout)
     }
 }
