e5bf9daeb446671080b566005ab65e5ea5d8e307
Egor Kisel <kisel1989@mail.ru>
2024-08-22 12:19:28 +0300

    tutor-38 переделал диалог, почистил стили, почистил граф

--- app/src/main/java/org/threehundredtutor/presentation/solution/PhotoDetailsFragment.kt
+++ app/src/main/java/org/threehundredtutor/presentation/solution/PhotoDetailsFragment.kt
@@ -1,15 +1,14 @@
 package org.threehundredtutor.presentation.solution
 
-import android.app.Dialog
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.FragmentManager
 import org.threehundredtutor.core.UiCoreAttr
 import org.threehundredtutor.ui_common.EMPTY_STRING
-import org.threehundredtutor.ui_common.UiCoreStyle
-import org.threehundredtutor.ui_common.fragment.applyWindowColor
-import org.threehundredtutor.ui_common.fragment.dropWindowColor
+import org.threehundredtutor.ui_common.fragment.getColorAttr
 import org.threehundredtutor.ui_common.util_class.BundleString
 import org.threehundredtutor.ui_common.view_components.loadImageOriginal
 import org.threehundredtutor.ui_core.databinding.FragmentPhotoDetailsBinding
@@ -19,28 +18,34 @@ class PhotoDetailsFragment : DialogFragment() {
     private lateinit var binding: FragmentPhotoDetailsBinding
     private var imagePath by BundleString(PHOTO_DETAILED_IMAGE_PATH, EMPTY_STRING)
 
-    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        binding = FragmentPhotoDetailsBinding.inflate(layoutInflater)
-        applyWindowColor(UiCoreAttr.defaultBlack)
-        val dialog = Dialog(requireContext(), UiCoreStyle.PhotoDialogStyle)
-        dialog.setContentView(binding.root)
-        dialog.window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
-        return dialog
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setStyle(STYLE_NORMAL, -1)
+        super.onCreate(savedInstanceState)
     }
 
-    override fun onStart() {
-        super.onStart()
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentPhotoDetailsBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         with(binding) {
             photoView.loadImageOriginal(imagePath)
-            photoViewToolBar.setNavigationOnClickListener {
-                dismiss()
-            }
+            photoViewToolBar.setNavigationOnClickListener { dismiss() }
         }
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        dropWindowColor()
+    override fun onResume() {
+        super.onResume()
+        requireDialog().window?.apply {
+            statusBarColor = getColorAttr(UiCoreAttr.defaultBlack, false)
+            navigationBarColor = getColorAttr(UiCoreAttr.defaultBlack, false)
+        }
     }
 
     companion object {
